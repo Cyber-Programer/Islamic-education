ffac672e635f4802bc1175732229412d9fa5479e
Cyber-Programer <sifat706401@gmail.com>
2024-03-06 12:24:46 -0500

    update 12:24

---
+++ loading.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        const loader = document.querySelector('.loader-wrapper');
+        loader.style.display = 'none';
+    }, 200); // Adjust the delay time in milliseconds (e.g., 1000 milliseconds = 1 second)
+});
